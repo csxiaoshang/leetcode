73d2f163daad4371dfad349a7f78d85bb12e61b6
csxiaoshang <970090853@qq.com>
2019-01-15 21:00:21 +0800

    2019.1.15

---
+++ src/main/java/AssignCookies.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public int findContentChildren(int[] g,int[] s){
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int glength=g.length;
+        int slength=s.length;
+        int i=0,j=0;
+        int count=0;
+        while(i<glength&&j<slength){
+            if (s[j]>g[i]){
+                i++;
+                j++;
+                count++;
+            }else {
+                j++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/MinimumNumberofArrowstoBurstBallons.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class MinimumNumberofArrowstoBurstBallons {
+    public  int findMiniArrowsShots(int[][] points){
+
+    }
+}
