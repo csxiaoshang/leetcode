2eece6c3b4ec725f9ab12ac058f2c3f9f03b3126
csxiaoshang <970090853@qq.com>
2019-02-25 21:28:11 +0800

    test

--- src/main/java/Subsequence392.java
+++ src/main/java/Subsequence392.java
@@ -6,6 +6,13 @@ package main.java;
  * @Description TODO
  **/
 public class Subsequence392 {
+    public AssignCookies assignCookies;
+    public Subsequence392(AssignCookies assignCookies){
+        this.assignCookies=assignCookies;
+    }
+    public void play(){
+        assignCookies.play();
+    }
     public boolean isSubsequence(String s,String t){
         for (int i = 0, pos = 0; i < s.length(); i++, pos++) {
             pos = t.indexOf(s.charAt(i), pos);
