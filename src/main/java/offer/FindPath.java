134080e80e920c0fb68408859428473915e802e1
csxiaoshang <970090853@qq.com>
2019-04-02 19:49:51 +0800

    19-4-02

---
+++ src/main/java/offer/FindPath.java
@@ -0,0 +1,25 @@
+package main.java.offer;
+
+import java.util.ArrayList;
+
+/**
+ * @author ashang  liuys@bupt.edu.cn
+ * @Date 19-4-2 下午7:47
+ * <p>
+ * 类说明：
+ */
+public class FindPath {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+        public class Solution {
+            public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+
+            }
+}
--- src/main/java/offer/VerifySqueceOfBST.java
+++ src/main/java/offer/VerifySqueceOfBST.java
@@ -8,8 +8,14 @@ package main.java.offer;
  */
 public class VerifySqueceOfBST {
     public boolean VerifySquenceOfBST(int [] sequence) {
-
+        if (sequence.length==0) return false;
+        return compare(sequence,0,sequence.length-1);
     }
-    public boolean compare()
+    public boolean compare(int [] tem,int l,int r){
+        if (l>=r) return true;
+        int i=r;
+        while (i>l&&tem[i-1]>tem[r]) --i;
+        for (int j=i-1;j>=l;--j) if (tem[j]>tem[r]) return false;
+        return compare(tem,l,i-1)&&(compare(tem,i,r-1));
+    }
 }
-g
