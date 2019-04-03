9ce8469c23bb2a765760eb0e7a556662f47297ce
csxiaoshang <970090853@qq.com>
2019-04-03 17:37:32 +0800

    19-4-3

--- src/main/java/offer/FindPath.java
+++ src/main/java/offer/FindPath.java
@@ -16,10 +16,23 @@ public class FindPath {
 
         public TreeNode(int val) {
             this.val = val;
-
         }
-        public class Solution {
-            public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
 
+        public class Solution {
+            private ArrayList<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
+            private ArrayList<Integer> list = new ArrayList<Integer>();
+
+            public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+                if (root == null) return listAll;
+                list.add(root.val);
+                target -= root.val;
+                if (target == 0 && root.left == null && root.right == null)
+                    listAll.add(new ArrayList<Integer>(list));
+                FindPath(root.left, target);
+                FindPath(root.right, target);
+                list.remove(list.size() - 1);
+                return listAll;
             }
+        }
+    }
 }
