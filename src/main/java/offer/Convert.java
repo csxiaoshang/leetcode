c809864183323b3bd456b3834b815f1bef11e074
csxiaoshang <970090853@qq.com>
2019-04-09 22:04:10 +0800

    add convert tree

--- src/main/java/offer/Convert.java
+++ src/main/java/offer/Convert.java
@@ -16,7 +16,24 @@ public class Convert {
             this.val = val;
 
         }
+
         public TreeNode Convert(TreeNode pRootOfTree) {
-            if (pRootOfTree==null) return null;
+            if (pRootOfTree == null) return null;
+            if (pRootOfTree.left == null && pRootOfTree.right == null) return pRootOfTree;
+            TreeNode left = Convert(pRootOfTree.left);
+            TreeNode p = left;
+            while (p != null && p.right != null)
+                p = p.right;
+            if (left != null) {
+                p.right = pRootOfTree;
+                pRootOfTree.left = p;
+            }
+            TreeNode right = Convert(pRootOfTree.right);
+            if (right != null) {
+                right.left = pRootOfTree;
+                pRootOfTree.right = right;
+            }
+            return left != null ? left : pRootOfTree;
         }
+    }
 }
