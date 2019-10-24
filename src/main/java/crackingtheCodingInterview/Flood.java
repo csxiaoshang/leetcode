885b36646a7b92351c4bb8a484316f11f4184934
csxiaoshang <970090853@qq.com>
2019-10-24 21:48:41 +0800

    add flood

---
+++ src/main/java/crackingtheCodingInterview/Flood.java
@@ -0,0 +1,14 @@
+package main.java.crackingtheCodingInterview;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 19-10-23 下午8:45
+ * <p>
+ * 类说明：
+ */
+public class Flood {
+    public int floodFill(int[][] map, int n, int m) {
+        // write code here
+        return n+m-2;
+    }
+}
