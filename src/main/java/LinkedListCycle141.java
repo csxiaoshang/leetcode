0ff4be224ad57793d16c9401ed3665a58b510ebf
csxiaoshang <970090853@qq.com>
2019-12-13 11:31:20 +0800

    add 141 21

---
+++ src/main/java/LinkedListCycle141.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.util.HashSet;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 19-12-13 上午11:19
+ * <p>
+ * 类说明：
+ */
+public class LinkedListCycle141 {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while(head != null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
