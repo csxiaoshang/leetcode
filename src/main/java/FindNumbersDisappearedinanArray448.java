23c1d0ddaaa32c597ece3ce226f1792ffdadfd68
csxiaoshang <970090853@qq.com>
2019-12-25 09:44:50 +0800

    add subTree

--- src/main/java/FindNumbersDisappearedinanArray448.java
+++ src/main/java/FindNumbersDisappearedinanArray448.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author ashang  970090853@qq.com
  * @Date 19-12-24 上午9:40
@@ -7,4 +10,30 @@ package main.java;
  * 类说明：
  */
 public class FindNumbersDisappearedinanArray448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+
+        LinkedList<Integer> list = new LinkedList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int tem = nums[i];
+                set(tem, nums);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
+
+    void set(int tem, int[] nums) {
+        if (nums[tem - 1] == 0)
+            return;
+        int swap = nums[tem - 1];
+        nums[tem - 1] = 0;
+        set(swap, nums);
+    }
 }
