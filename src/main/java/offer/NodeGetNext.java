cdc40822944dd51624c1b656b0c8e34fc6ba0433
csxiaoshang <970090853@qq.com>
2020-02-27 23:24:44 +0800

    add tree so on

---
+++ src/main/java/offer/NodeGetNext.java
@@ -0,0 +1,30 @@
+package main.java.offer;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 20-2-26 上午9:40
+ * <p>
+ * 类说明：
+ */
+public class NodeGetNext {
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if(pNode == null){
+            return pNode;
+        }
+        if(pNode.right!=null){
+            pNode = pNode.right;
+            while(pNode.left != null)
+                pNode = pNode.left;
+            return pNode;
+        }else{
+            while(pNode.next!=null){
+                if(pNode.next.left == pNode){
+                    return pNode.next;
+                }
+                pNode = pNode.next;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/offer/TreeNode.java
@@ -0,0 +1,18 @@
+package main.java.offer;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 20-2-27 下午10:30
+ * <p>
+ * 类说明：
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
