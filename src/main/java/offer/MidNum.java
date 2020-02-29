e6b5afc6245d344f5fd715a9e5d81b4af0793dcc
csxiaoshang <970090853@qq.com>
2020-02-29 23:27:21 +0800

    mid num

---
+++ src/main/java/offer/MidNum.java
@@ -0,0 +1,47 @@
+package main.java.offer;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 20-2-29 下午7:51
+ * <p>
+ * 类说明：
+ */
+public class MidNum {
+    int count = 0;
+    //小顶堆
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    //大顶堆
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(15, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
+    public void Insert(Integer num) {
+        //个数为偶数的话，则先插入到大顶堆，然后将大顶堆中最大的数插入小顶堆中
+        if(count % 2 == 0){
+            maxHeap.offer(num);
+            int max = maxHeap.poll();
+            minHeap.offer(max);
+        }else{
+            //个数为奇数的话，则先插入到小顶堆，然后将小顶堆中最小的数插入大顶堆中
+            minHeap.offer(num);
+            int min = minHeap.poll();
+            maxHeap.offer(min);
+        }
+        count++;
+    }
+
+    public Double GetMedian() {
+        //当前为偶数个，则取小顶堆和大顶堆的堆顶元素求平均
+        if(count % 2 == 0){
+            return new Double(minHeap.peek() + maxHeap.peek())/2;
+        }else{
+            //当前为奇数个，则直接从小顶堆中取元素即可
+            return new Double(minHeap.peek());
+        }
+    }
+}
