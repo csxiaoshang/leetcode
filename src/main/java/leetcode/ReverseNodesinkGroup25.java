f3a27f00343f3222d054908da72cb7dc9a34f1b8
csxiaoshang <970090853@qq.com>
2020-05-20 22:10:42 +0800

    05/20

--- src/main/java/leetcode/ReverseNodesinkGroup25.java
+++ src/main/java/leetcode/ReverseNodesinkGroup25.java
@@ -10,7 +10,48 @@ package main.java.leetcode;
  */
 public class ReverseNodesinkGroup25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+        if (head == null) {
+            return head;
+        }
+        ListNode res = new ListNode(-1);
+        ListNode index = res;
+        ListNode slow = head,fast = head;
+        if (k == 1) {
+            while (slow != null) {
+                ListNode tem = slow.next;
+                slow.next = res.next;
+                res.next = slow;
+                slow = tem;
+            }
+            return res.next;
+        }
+        while (fast.next != null) {
+            int count = 0;
+            while (fast.next != null) {
+                fast = fast.next;
+                count++;
+                if (count >= k -1) {
+                    break;
+                }
+            }
+            fast = fast.next;
+            if (count < k -1) {
+                break;
+            }
+            ListNode cur = slow;
+            while (count-->=0) {
+                ListNode temp = slow.next;
+                slow.next = index.next;
+                index.next = slow;
+                slow = temp;
+            }
+            index = cur;
+            if (fast == null){
+                break;
+            }
+        }
+        index.next = slow;
+        return res.next;
     }
 }
 
--- src/main/java/leetcode/ZigZagConversion6.java
+++ src/main/java/leetcode/ZigZagConversion6.java
@@ -1,5 +1,8 @@
 package main.java.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ashang  970090853@qq.com
  * @Date 20-3-25 上午9:01
@@ -7,7 +10,36 @@ package main.java.leetcode;
  * 类说明：
  */
 public class ZigZagConversion6 {
-    public String convert(String s, int numRows) {
-        return null;
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 4));;
     }
+    public static String convert(String s, int numRows) {
+
+        if (numRows == 1) {
+            return s;
+        }
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) {
+            ret.append(row);
+        }
+        return ret.toString();
+    }
+
 }
