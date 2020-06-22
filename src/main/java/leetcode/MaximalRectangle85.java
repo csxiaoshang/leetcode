2eaa8f6a21a3d665c22a1824ea9749f5d9df005d
csxiaoshang <970090853@qq.com>
2020-06-22 08:16:49 +0800

    06/22

--- src/main/java/leetcode/MaximalRectangle85.java
+++ src/main/java/leetcode/MaximalRectangle85.java
@@ -23,4 +23,45 @@ public class MaximalRectangle85 {
     public int maximalRectangle(char[][] matrix) {
         return 0;
     }
+
+    /**
+     * 暴力解法　success
+     */
+    class Solution1{
+        public int maximalRectangle(char[][] matrix) {
+            int maxArea = 0;
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return maxArea;
+            }
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[][] num = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == '1') {
+                        if (j == 0) {
+                            num[i][j] = 1;
+                        }else{
+                            num[i][j] = num[i][j - 1] + 1;
+                        }
+                    }else{
+                        num[i][j] = 0;
+                    }
+
+                    int minWith = num[i][j];
+                    for (int k = i; k >= 0; k--) {
+                        int height = i-k+1;
+                        minWith = Math.min(minWith, num[k][j]);
+                        maxArea = Math.max(maxArea, height * minWith);
+                    }
+                }
+            }
+            return maxArea;
+        }
+    }
+    class Solution2{
+        public int maximalRectangle(char[][] matrix) {
+
+        }
+    }
 }
