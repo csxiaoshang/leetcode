c2f95fa4339d271b79e0909ca9cf10562ac436a8
csxiaoshang <970090853@qq.com>
2020-09-13 00:43:35 +0800

    09/12

--- src/main/java/leetcode/MinimumDepthofBinaryTree111.java
+++ src/main/java/leetcode/MinimumDepthofBinaryTree111.java
@@ -1,5 +1,7 @@
 package main.java.leetcode;
 
+import java.util.LinkedList;
+
 /**
  * @author ashang  970090853@qq.com
  * @Date 20-4-27 上午11:29
@@ -7,6 +9,12 @@ package main.java.leetcode;
  * 类说明：
  */
 public class MinimumDepthofBinaryTree111 {
+
+    /**
+     * dfs
+     * @param root
+     * @return
+     */
     public int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
@@ -16,4 +24,39 @@ public class MinimumDepthofBinaryTree111 {
         if (left == 0 || right == 0) return left + right + 1;
         return Math.min(left, right) + 1;
     }
+
+    /**
+     * bfs
+     * @param root
+     * @return
+     */
+    public int minDepth1(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+
+                if (cur.left == null && cur.right == null) {
+                    return res;
+                }
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            res++;
+        }
+        return res;
+    }
+
 }
