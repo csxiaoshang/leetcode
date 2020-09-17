3fe479275d71c778527fe6ef66bda7d06d3b2b39
csxiaoshang <970090853@qq.com>
2020-09-17 23:58:39 +0800

    09/17

---
+++ src/main/java/leetcode/BestTimetoBuyandSellStockIV188.java
@@ -0,0 +1,60 @@
+package main.java.leetcode;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 20-9-15 下午5:27
+ * <p>
+ * 类说明：
+ * Say you have an array for which the i-th element is the price of a given stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete at most k transactions.
+ *
+ * Note:
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ *
+ * Example 1:
+ *
+ * Input: [2,4,1], k = 2
+ * Output: 2
+ * Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+ * Example 2:
+ *
+ * Input: [3,2,6,5,0,3], k = 2
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+ *              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ */
+public class BestTimetoBuyandSellStockIV188 {
+
+    public int maxProfit(int k, int[] prices) {
+
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        if (k > n / 2) {
+            k = n / 2 + 1;
+        }
+        int[][][] dp = new int[n][k+1][2];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0][0] = 0;
+            dp[i][0][1] = Integer.MIN_VALUE;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+
+                if (i == 0) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[i];
+                    continue;
+                }
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][k][0];
+    }
+}
---
+++ src/main/java/leetcode/LongestPalindromicSubsequence516.java
@@ -0,0 +1,14 @@
+package main.java.leetcode;
+
+/**
+ * @author ashang  970090853@qq.com
+ * @Date 20-9-17 下午11:32
+ * <p>
+ * 类说明：
+ */
+public class LongestPalindromicSubsequence516 {
+
+    public int longestPalindromeSubseq(String s) {
+
+    }
+}
